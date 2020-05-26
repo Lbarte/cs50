a303e36c6ed611e610393396177bc504fed1d97c
Lbarte <Lbarte@users.noreply.github.com>
2020-05-26 21:38:24 +0000

    automated commit by check50 [check50=True]

--- hello.c
+++ hello.c
@@ -1,8 +1,8 @@
-//hello world with a name attached
+//hello with a name attached
 #include <stdio.h>
 #include <cs50.h>
 
 int main(void)
 {
-    printf("hello world, %s\n", get_string("what is you name?\n"));
+    printf("hello, %s\n", get_string("what is you name?\n"));
 }
