6bdc684cfa24bf8e957dd0544a6be0d9cd3d5209
Lbarte <Lbarte@users.noreply.github.com>
2020-05-26 22:36:45 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -6,9 +6,15 @@ int main(void)
 {
     int userNumber;
     printf("A number between 1 and 8 inclusive\n");
-    do {
+
+    //check if number is between 1 and 8 inclusive
+    do
+    {
         userNumber = get_int("Height: ");
-    } while (userNumber < 1 || userNumber > 8);
+    }
+    while (userNumber < 1 || userNumber > 8);
+
+    //printing pyramid
     for (int i = 1; i <= userNumber; i++)
     {
         for (int spaceCounter = userNumber - i; spaceCounter > 0; spaceCounter--)
