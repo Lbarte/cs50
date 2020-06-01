ed474b54bc7df1310c0749888a9a31a13df317ef
Lbarte <Lbarte@users.noreply.github.com>
2020-06-01 22:05:54 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -22,49 +22,38 @@ int main(void)
     // starting greedy algorithm
     do
     {
-        if ((coinsOwed / 50) > 0)
+        if ((coinsOwed / 25) > 0)
         {
-            // we can give some rest in 50's coin
-            // how much 50's coins can we give?
-            coinsWeCanGive = coinsOwed - (coinsOwed % 50);
-            coinsCounter += coinsWeCanGive / 50;
+            // we can give some rest in 25's coin
+            // how much 25's coins can we give?
+            coinsWeCanGive = coinsOwed - (coinsOwed % 25);
+            coinsCounter += coinsWeCanGive / 25;
             coinsOwed -= coinsWeCanGive;
         } else
         {
-            // we can't give any rest in 50's coin
-            if ((coinsOwed / 25) > 0)
+            // we can't give any rest in 25's coin
+            if ((coinsOwed / 10) > 0)
             {
-                // we can give some rest in 25's coin
-                // how much 25's coins can we give?
-                coinsWeCanGive = coinsOwed - (coinsOwed % 25);
-                coinsCounter += coinsWeCanGive / 25;
+                // we can give some rest in 10's coin
+                // how much 10's coins can we give?
+                coinsWeCanGive = coinsOwed - (coinsOwed % 10);
+                coinsCounter += coinsWeCanGive / 10;
                 coinsOwed -= coinsWeCanGive;
             } else
             {
-                // we can't give any rest in 25's coin
-                if ((coinsOwed / 10) > 0)
+                // we can't give any rest in 10's coin
+                if ((coinsOwed / 5) > 0)
                 {
-                    // we can give some rest in 10's coin
-                    // how much 10's coins can we give?
-                    coinsWeCanGive = coinsOwed - (coinsOwed % 10);
-                    coinsCounter += coinsWeCanGive / 10;
+                    // we can give some rest in 5's coin
+                    // how much 5's coins can we give?
+                    coinsWeCanGive = coinsOwed - (coinsOwed % 5);
+                    coinsCounter += coinsWeCanGive / 5;
                     coinsOwed -= coinsWeCanGive;
                 } else
                 {
-                    // we can't give any rest in 10's coin
-                    if ((coinsOwed / 5) > 0)
-                    {
-                        // we can give some rest in 5's coin
-                        // how much 5's coins can we give?
-                        coinsWeCanGive = coinsOwed - (coinsOwed % 5);
-                        coinsCounter += coinsWeCanGive / 5;
-                        coinsOwed -= coinsWeCanGive;
-                    } else
-                    {
-                        // we can't give any rest in 5's coin
-                        coinsCounter += coinsOwed;
-                        coinsOwed = 0;
-                    }
+                    // we can't give any rest in 5's coin
+                    coinsCounter += coinsOwed;
+                    coinsOwed = 0;
                 }
             }
         }
